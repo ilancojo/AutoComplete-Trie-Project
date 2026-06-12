12ee95d2baeb86bd22d4a3046f8db4a1ebdc7c0d
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-13 00:33:05 +0300

    adding first test for AutoCompleteTrie.test

--- tests/AutoCompleteTrie.test.js
+++ tests/AutoCompleteTrie.test.js
@@ -11,7 +11,20 @@ describe('AutoCompleteTrie', () => {
 
     describe('addWord()', () => {
         
-       
+        test('should add a word and set endOfWord to true at the last character', () => {
+            trie.addWord('cat');
+            
+            // מוודאים שהשרשרת c -> a -> t קיימת
+            expect(trie.root.children['c']).toBeDefined();
+            expect(trie.root.children['c'].children['a']).toBeDefined();
+            expect(trie.root.children['c'].children['a'].children['t']).toBeDefined();
+            
+            // מוודאים שרק האות האחרונה מוגדרת כסוף מילה
+            expect(trie.root.children['c'].endOfWord).toBe(false);
+            expect(trie.root.children['c'].children['a'].children['t'].endOfWord).toBe(true);
+        });
+
+
 
     });
 });

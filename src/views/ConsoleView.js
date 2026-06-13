977c9d835e8c461385249dae1167ccd9743b04a8
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-14 00:33:14 +0300

    changing consoleView.js

--- src/views/ConsoleView.js
+++ src/views/ConsoleView.js
@@ -2,7 +2,6 @@ import promptSync from 'prompt-sync';
 
 export class ConsoleView {
     constructor() {
-        // אתחול ספריית הקלט. ההגדרה sigint מאפשרת למשתמש לצאת בעזרת Ctrl+C
         this.prompt = promptSync({ sigint: true }); 
     }
 
@@ -12,13 +11,5 @@ export class ConsoleView {
 
     getUserInput(promptText) {
         return this.prompt(promptText);
-    }
-
-    displayPredictions(predictions) {
-        if (predictions.length === 0) {
-            this.displayMessage("No perfections found.");
-        } else {
-            this.displayMessage(`Possible completions: [ ${predictions.join(', ')} ]`);
-        }
     }
 }
